ecae37bc91d8e2e511145856a34e160ed3dd5cb6
ashish-kumar-khatri <ashishkmrkhatri19@gmail.com>
2022-11-19 17:46:32 +0545

    :art:added seller register and login route with components

---
+++ src/pages/SellerLoginPage/SellerLoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SellerLoginPage.css'
+
+function SellerLoginPage(){
+
+	return(
+		<div>
+			SellerLoginPage
+		</div>
+	)
+
+}
+
+export default SellerLoginPage;
---
+++ src/pages/SellerRegisterPage/SellerRegisterPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SellerRegisterPage.css'
+
+function SellerRegisterPage(){
+
+	return(
+		<div>
+			SellerRegisterPage
+		</div>
+	)
+
+}
+
+export default SellerRegisterPage;
