e04f4b092ece9b9cd28cc252330d48a6f6b45960
ashish-kumar-khatri <ashishkmrkhatri19@gmail.com>
2022-12-12 02:27:56 +0545

    :rocket:changed base url for deployment

--- src/utils/endpoints/baseURL.js
+++ src/utils/endpoints/baseURL.js
@@ -1,8 +1,8 @@
-const isDev = true;
+const isDev = false;
 const urlConfig = {
   // dev: 'http://127.0.0.1:8000',
   dev: 'http://192.168.1.80:8000',
-  // prod: 'https://alldokan.cyclic.app',
+  prod: 'https://alldokan.cyclic.app',
 };
 const fetchBaseURL = () => (isDev ? urlConfig.dev : urlConfig.prod);
 
