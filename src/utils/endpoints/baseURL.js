e04aa1a246ae161e88f64157791469107502598e
ashish-kumar-khatri <ashishkmrkhatri19@gmail.com>
2022-12-28 00:27:54 +0545

    :lipstick:some styling

--- src/utils/endpoints/baseURL.js
+++ src/utils/endpoints/baseURL.js
@@ -1,11 +1,11 @@
 const isProduction = process.env.REACT_APP_PRODUCTION;
 const urlConfig = {
-  // dev: 'http://127.0.0.1:8000',
-  dev: 'http://192.168.1.80:8000',
+  dev: 'http://127.0.0.1:8000',
+  // dev: 'http://192.168.1.80:8000',
   prod: 'https://alldokan.cyclic.app',
 };
 
 // const baseURL = (isProduction ? urlConfig.dev : urlConfig.prod);
-const baseURL = (urlConfig.prod);
+const baseURL = (urlConfig.dev);
 
 export default baseURL;
